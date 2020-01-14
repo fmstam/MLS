7211bee6af31feae3f0a050bdc6288fd53b86190
faroq <faroq.al.tam@gmail.com>
2020-01-14 14:26:45 +0000

    initial commit

---
+++ utl/ReplayMemory.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+    An experience  reply memory class 
+  
+    It can support less than four headers and can be extended easily to support further (or less) columns.
+"""
+__author__ = "AL-Tam Faroq"
+__copyright__ = "Copyright 2019, UALG"
+__credits__ = ["Faroq AL-Tam"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Faroq AL-Tam"
+__email__ = "ftam@ualg.pt"
+__status__ = "Production"
